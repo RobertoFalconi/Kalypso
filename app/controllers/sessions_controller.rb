0d411ce65fd97ca1ec856ffc344974fbca882957
Roberto Falconi <falkons@outlook.com>
2017-06-30 16:58:27 +0200

    fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       #log the user in and redirect to the user's show page
       log_in user
+      remember user
       redirect_to user # -> user_url(user)
     else
        flash.now[:danger] = 'Invalid email/password combination'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,11 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+
   #Returns the current logged-in user (if any)
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
