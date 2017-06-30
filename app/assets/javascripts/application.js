2f00e562b12dad8b409d654f994a6fa4563963c7
Roberto Falconi <falkons@outlook.com>
2017-06-30 15:56:55 +0200

    bugs fixed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery_ujs
 //= require bootstrap
+//= require_tree .
 //= require turbolinks
 //= require_tree
 //= require rails-ujs
-
 
