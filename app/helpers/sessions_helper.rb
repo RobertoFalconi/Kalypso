333166430e1cf7e71f1ad8893f84f6f76b908070
Roberto Falconi <falkons@outlook.com>
2017-07-01 13:24:40 +0200

    fixed logout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,43 +11,18 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
-  # Forgets a persistent session.
-  def forget(user)
-    user.forget
-    cookies.delete(:user_id)
-    cookies.delete(:remember_token)
-  end
-
-  # Logs out the current user.
-  def log_out
-    current_user && forget(current_user)  
-    session.delete(:user_id)
-    @current_user = nil
-  end
-  
-
-
   #Returns the current logged-in user (if any)
   def current_user
-	  @current_user ||= User.find_by(id: session[:user_id])
-    if(user_id = session[:user_id])
-      @current_user ||= User.find_by(id: :user_id)
-    elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id:user_id)
-      if user && user.authenticated?(cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
-    end
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
   def logged_in?
     !current_user.nil?
   end
 
-	def log_out
-     session.delete(:user_id)
-     @current_user = nil
-   end
-	
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
 end
