54dc3cbbc5ba21d9229226714b29f3977a56c0eb
Salvatore Licitra <salvolicitra95@gmail.com>
2017-07-25 09:26:57 +0200

    minor fixes

--- test/controllers/subscribers_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class SubscribersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get subscribers_path
-    assert_response :success
-  end
-
-end
